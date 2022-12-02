ac0824b898d93eea46e7b93fe205e1dda8f954e0
Boerilorenzo03 <117928573+Boerilorenzo03@users.noreply.github.com>
2022-12-02 15:47:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,14 @@
-#include <iostream>
+#include<iostream>
 #include "lib.h"
-int main(){
-  int h;
-  std::cin >> h;
-  if (numeroprimo(h) == 0) {
-    std::cout << "numero primo" << std::endl;
-  }
-  else{
-    std::cout << "numero non primo" << std::endl;
-  }
-  return 0;
-}
-
-#include <iostream>
-#include "lib.h"
-using namespace std;
-bool numeroprimo (int n) {
-  int i=1, x;
-  bool risultato;
-  if (n>1){
-    do{
-      i++;
-      x = n%i;
+int main() {
+    int h;
+    std::cin >> h;
+    if (numeroprimo(h) == 0) {
+        std::cout << "numero primo" << std::endl;
     }
-    while (x!=0);
-    if (n == i) {
-      risultato = 0;
-      ruturn risultato;
+    else {
+        std::cout << "numero non primo" << std::endl;
     }
-    else{
-      risultato = 1;
-      return risultato;
-    }
-  }
+    return 0;
 }
       
